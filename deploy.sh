355c621e5bdeaf44cbd6bc089319694d2576c450
thunderBiscuit <thunderB@protonmail.com>
2020-04-03 12:42:11 -0400

    update deploy script to reflect new repo name

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:thunderBiscuit/mynode-test-docs.git master:gh-pages
+git push -f git@github.com:thunderBiscuit/mynode-docs-vuepress-mockup.git master:gh-pages
 
 cd -
